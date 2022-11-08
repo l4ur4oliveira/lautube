f5eefd493b6a2a1f8f6aea117da20e58cf97711d
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-08 15:02:32 -0300

    Add styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import config from '../config.json';
+import styled from 'styled-components';
 
 function HomePage() {
   return (
@@ -18,9 +19,32 @@ function Menu() {
   );
 }
 
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .user-info {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    width: 100%;
+    padding: 16px 32px;
+  }
+`;
 function Header() {
   return (
-    <div>Header</div>
+    <StyledHeader>
+      <img src="banner" />
+      <div className='user-info'>
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </div>
+    </StyledHeader>
   );
 }
 
