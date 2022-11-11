36d0bb7f7d8e3aea7a6cf684b53871c4d9252c72
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-10 22:24:10 -0300

    Add form states

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -2,7 +2,12 @@ import { useState } from "react";
 import { StyledRegisterVideo } from "./styles";
 
 export default function RegisterVideo() {
-  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [isFormVisible, setIsFormVisible] = useState(true);
+  const [values, setValues] = useState({ title: "", url: ""});
+
+  function temporarySubmit() {
+    alert(`${values.title} - ${values.url}`);
+  }
 
   return (
     <StyledRegisterVideo>
@@ -12,19 +17,41 @@ export default function RegisterVideo() {
           onClick={() => setIsFormVisible(true)}
         >+</button>
         {
-          (isFormVisible) ?
-            <form>
+          isFormVisible
+          ? (
+            <form onSubmit={(e) => e.preventDefault()}>
               <div>
                 <button
                   type="button"
                   className="close-modal"
                   onClick={() => setIsFormVisible(false)}
                 >X</button>
-                <input placeholder="Título do vídeo" />
-                <input placeholder="URL" />
-                <button type="submit">Cadastrar</button>
+                <input
+                  placeholder="Título do vídeo"
+                  name="video-title"
+                  value={values.title}
+                  onChange={(e) => {
+                    setValues({
+                      ...values,
+                      title: e.target.value
+                    });
+                  }}
+                />
+                <input
+                  placeholder="URL"
+                  name="video-url"
+                  value={values.url}
+                  onChange={(e) => {
+                    setValues({
+                      ...values,
+                      url: e.target.value
+                    });
+                  }}
+                />
+                <button type="submit" onClick={temporarySubmit}>Cadastrar</button>
               </div>
             </form>
+          )
           : null
         }
       </div>
