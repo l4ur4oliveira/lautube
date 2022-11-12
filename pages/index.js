82a3a4c52a0422808ab3ecdcccae2ebb3956bdc5
Laura de Oliveira <l4ur4.88@gmail.com>
2022-11-11 23:38:59 -0300

    Add Supabase integration

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
-import { useState } from "react";
-
-import config from "../config.json";
+import { useEffect, useState } from "react";
 
 import Header from "../src/components/Header";
 import Menu from "../src/components/Menu";
 import Timeline from "../src/components/Timeline";
 
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl  = "https://wrboteehgyqavoyfqjpq.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl , supabaseKey);
+
 export default function HomePage() {
   const [ searchValue, setSearchValue ] = useState("");
+  const [playlists, setPlaylists] = useState({});
+
+  useEffect(() => {
+    console.log("USE EFFECT");
+
+    supabase.from("video")
+      .select("*")
+      .then((response) => {
+        const newPlaylists = {...playlists};
+        
+
+        response.data.forEach((item) => {
+          if(!newPlaylists[item.playlist]) {
+            newPlaylists[item.playlist] = [];
+          }
+          newPlaylists[item.playlist].push(item);
+        })
+        setPlaylists(newPlaylists);
+      });
+  },[]);
 
   return (
     <>
@@ -19,7 +43,7 @@ export default function HomePage() {
         <Menu searchValue={searchValue} setSearchValue={setSearchValue} />
         <main>
           <Header />
-          <Timeline searchValue={searchValue} playlists={config.playlists} />
+          <Timeline searchValue={searchValue} playlists={playlists} />
         </main>
       </div>
     </>
--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { StyledRegisterVideo } from "./styles";
+import { createClient } from "@supabase/supabase-js";
 
 function useForm(formProps) {
   const [values, setValues] = useState(formProps.initialValues);
-
   
   return {
     values,
     handleChange: (e) => {
       const name = e.target.name;
-
+      
       setValues({
         ...values,
         [name]: e.target.value
@@ -19,10 +19,23 @@ function useForm(formProps) {
   }
 }
 
+const supabaseUrl  = "https://wrboteehgyqavoyfqjpq.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl , supabaseKey);
+
+function getVideoId(url) {
+  const videoId = url.split("v=")[1];
+  const ampersandPosition = videoId.indexOf("&");
+  if (ampersandPosition !== -1) {
+    return videoId.substring(0, ampersandPosition);
+  }
+  return videoId;
+}
+
 export default function RegisterVideo() {
   const [isFormVisible, setIsFormVisible] = useState(false);
   const formSubmit = useForm({
-    initialValues: { title: "", url: "" }
+    initialValues: { title: "", url: "", playlist: "" }
   });
 
   return (
@@ -37,6 +50,22 @@ export default function RegisterVideo() {
           ? (
             <form onSubmit={(e) => {
               e.preventDefault();
+
+              const videoId = getVideoId(formSubmit.values.url);
+
+              supabase.from("video").insert({
+                title: formSubmit.values.title,
+                url: formSubmit.values.url,
+                thumb: `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`,
+                playlist: formSubmit.values.playlist
+              })
+              .then((response) => {
+                console.log(response);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+
               formSubmit.clearForm();
               setIsFormVisible(false);
             }}>
@@ -46,6 +75,12 @@ export default function RegisterVideo() {
                   className="close-modal"
                   onClick={() => setIsFormVisible(false)}
                 >X</button>
+                <input
+                  placeholder="Título da playlist"
+                  name="playlist"
+                  value={formSubmit.values.playlist}
+                  onChange={formSubmit.handleChange}
+                />
                 <input
                   placeholder="Título do vídeo"
                   name="title"
